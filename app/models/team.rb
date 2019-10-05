9a43acf241c486820ffc3b4a2a2709f7442f9c73
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-05 20:16:08 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,14 +1,10 @@
 class Team
   
   attr_accessor :name, :motto
-  @@all = []
   
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
   end 
-  
-  def self.all
-    @@all 
-  end 
+
 end 
